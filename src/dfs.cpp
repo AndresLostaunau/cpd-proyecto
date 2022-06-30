d7f896d7b0a62f060d87b7fcd5cf504e23ab2d69
Oteranga <alejandro.otero@utec.edu.pe>
2022-06-30 00:10:43 -0500

    Sequential DFS done

--- src/dfs.cpp
+++ src/dfs.cpp
@@ -1,1 +1,61 @@
 #include "tsmp.cpp"
+
+using namespace std;
+
+class graph{
+    private:
+        vector<path> cities;
+        vector<int> order;
+    public:
+        graph();
+        void DFS(int index);
+        int closest_city(path city);
+        void print_route();
+
+};
+
+graph::graph(){
+    for(int i = 0; i < N; i++){
+        path temp_obj(i);
+        cities.push_back(temp_obj);
+    }
+}
+
+
+void graph::DFS(int index){
+    if(!cities[index].visited){
+        cities[index].visited = true;
+        order.push_back(index);
+        int adjacent_city = closest_city(cities[index]);
+        DFS(adjacent_city);
+    }
+    else{
+        for(int i = 0; i < N; i++){
+            if(!this->cities[i].visited)
+                DFS(i);
+        }
+    }
+}
+
+int graph::closest_city(path city){
+    int index_closest_city = 0;
+    for(int i = 0; i < N; i++){
+        if(city.route[index_closest_city] >= city.route[i])
+            index_closest_city = i;
+    }
+    return index_closest_city;
+}
+
+void graph::print_route(){
+    vector<int>::iterator it;
+    for(it = order.begin(); it != order.end(); it++)
+        cout << *it << " ";
+}
+
+int main(){
+    //graph g;
+    //g.DFS(0);
+    //g.print_route();
+
+    return 0;
+}
--- src/tsmp.cpp
+++ src/tsmp.cpp
@@ -39,3 +39,13 @@ struct pile_elem{
         reduced_value = rv;
     }
 };
+
+struct path{
+    bool visited = false;
+    double route[N];
+
+    path(int index){
+        for(int i = 0; i < N; i++)
+            route[i] = route_graph[index][i];
+    }
+};
